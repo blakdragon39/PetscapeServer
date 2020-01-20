641728e35aa26391fae4d5fac9357fcb2f3ba3f2
Jenna Hartley <jenna.hartley@pushinteractions.com>
2020-01-20 16:01:39 -0600

    Started moving old initial data into non-rel format

--- src/main/kotlin/com/petscape/server/PetscapeApplication.kt
+++ src/main/kotlin/com/petscape/server/PetscapeApplication.kt
@@ -1,8 +1,15 @@
 package com.petscape.server
 
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
 import io.dropwizard.Application
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
+import org.bson.codecs.configuration.CodecRegistries
+import org.bson.codecs.configuration.CodecRegistry
+import org.bson.codecs.pojo.PojoCodecProvider
+
 
 @Throws(Exception::class)
 fun main(args: Array<String>) {
@@ -11,14 +18,22 @@ fun main(args: Array<String>) {
 
 class PetscapeApplication : Application<PetscapeConfiguration>() {
 
+    lateinit var mongoClient: MongoClient
+
     override fun getName(): String {
         return "petscape-server"
     }
 
-    override fun initialize(bootstrap: Bootstrap<PetscapeConfiguration?>) { // nothing to do yet
+    override fun initialize(bootstrap: Bootstrap<PetscapeConfiguration?>) {
+        mongoClient = MongoClients.create()
+        val pojoCodecRegistry = CodecRegistries.fromRegistries(
+            MongoClientSettings.getDefaultCodecRegistry(),
+            CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
+        )
     }
 
     override fun run(configuration: PetscapeConfiguration, environment: Environment) {
+        environment.healthChecks().register()
         environment.jersey().register(HelloWorldResource(configuration.template, configuration.defaultName))
     }
 }
